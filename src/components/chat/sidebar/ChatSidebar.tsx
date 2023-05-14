fc4f3fa747bce0e8fe801f5e7a3ba16d3c84323e
ks6088ts <ks6088ts@gmail.com>
2023-05-15 07:50:34 +0900

    remove unnecessary UI components

--- src/components/chat/sidebar/ChatSidebar.tsx
+++ src/components/chat/sidebar/ChatSidebar.tsx
@@ -2,12 +2,9 @@ import Link from "next/link";
 import React from "react";
 import { MdAdd, MdDeleteOutline, MdBuild } from "react-icons/md";
 import { useOpenAI } from "@/context/OpenAIProvider";
-import Github from "../../misc/Github";
 import ThemeButton from "./buttons/ThemeButton";
 import ButtonContainer from "./buttons/ButtonContainer";
 import Conversations from "./conversation/Conversations";
-import ApiKey from "./buttons/ApiKey";
-import CurrentModel from './buttons/CurrentModel';
 
 type Props = {};
 
@@ -28,10 +25,6 @@ export default function ChatSidebar({}: Props) {
         <Conversations />
 
         <div className="flex flex-col gap-y-2 border-y border-white/10 py-2">
-          <div className="flex flex-col border-b border-white/10 gap-y-2">
-            <CurrentModel />
-            <ApiKey />
-          </div>
           <Link
             className="flex items-center gap-3 rounded p-3 transition-colors hover:bg-gray-500/10"
             href="/playground"
@@ -46,11 +39,6 @@ export default function ChatSidebar({}: Props) {
 
           <ThemeButton />
         </div>
-
-        <Github />
-        <span className="text-center text-primary/80">
-          Made with ❤️ by Nashex
-        </span>
       </div>
     </div>
   );
